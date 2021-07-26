112ade5b63812631a0ebffcf349b6c49a2ca328c
Michal Budzowski <michal.budzowski@sonarsource.com>
2021-07-26 15:00:36 +0200

    add files

---
+++ 11liner.sql
@@ -0,0 +1,11 @@
+select
+from
+tab1,
+tab2,
+tab3,
+tab4,
+tab5,
+tab6,
+tab7
+where
+cond is null
---
+++ 31liner-duplicated.sql
@@ -0,0 +1,31 @@
+select
+from
+tab1,
+tab2,
+tab3,
+tab4,
+tab5,
+tab6,
+tab7,
+tab8,
+tab9
+where 
+cond0 is null
+and
+cond1 is null 
+and 
+cond2 is null
+and 
+cond3 is null
+and 
+cond4 is null
+and 
+cond5 is null
+and 
+cond6 is null
+and 
+cond7 is null
+and 
+cond8 is null
+and 
+cond9 is null;
